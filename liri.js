2e6afe48133c64a6056bfc0bfd9e74a34f7b73a3
Eric Fuller <efuller82@gmail.com>
2019-07-31 12:08:28 -0500

    concert-this and moment now working

--- liri.js
+++ liri.js
@@ -57,3 +57,35 @@ if (searchMethod === "movie-this") {
             console.log(error.config);
         });
 }
+
+// Bands in town 
+if (searchMethod === "concert-this") {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
+    var axios = require("axios");
+    var moment = require("moment");
+    axios
+        .get(queryUrl)
+        .then(function (response) {
+            console.log("Venue name: " + response.data[0].venue.name);
+            console.log("City: " + response.data[0].venue.city);
+            console.log("Date: " + moment(response.data[0].datetime).format('MM/DD/YYYY'));
+
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
